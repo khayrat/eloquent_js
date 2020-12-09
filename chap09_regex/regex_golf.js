cfa1a7fbe4291dc46691a4ed97ce1efad03286cc
Khayrat Glende <khayrat.glende@neofonie.de>
2020-12-09 08:37:46 +0100

    added regex_golf.

--- chap09_regex/regex_golf.js
+++ chap09_regex/regex_golf.js
@@ -47,9 +47,43 @@ test("Any word ending with ious", () => {
 });
 
 // A whitespace character followed by a period, comma, colon, or semicolon
+test("A whitespace character followed by a period, comma, colon, or semicolon", () => {
+  let regex = /\s[.,;-]/;
+  let result = true
+    && "sdfsf .sfds".match(regex)
+    && "sdfsf ;sfds".match(regex)
+    && "sdfsf -sfds".match(regex)
+    && "sdfsf -sfds".match(regex)
+    && `sdfsf
+    -sfds`.match(regex)
+    && "sdfdsf ,sdfdsfsdf".match(regex);
+
+  return result;
+});
 
 // A word longer than six letters
+test("A word longer than six letters", () => {
+  let regex = /\b\w{6,}\b/;
+  let result = true
+    && !"12345".match(regex)
+    && "sdfs 123456 sdfds".match(regex)
+    && "1234567".match(regex)
+    && "123456".match(regex);
+
+  return result;
+});
 
 // A word without the letter e (or E)
+test("A word without the letter e (or E)", () => {
+  let regex = /\b[a-df-zA-DF-Z0-9]+\b/;
+  let result = true
+    && !"abcdefg".match(regex)
+    && !"ABCDEFG".match(regex)
+    && "sdfdsf sdfdsf dsfsdfsde".match(regex)
+    && "1234567 sfsddf".match(regex)
+    && "1e2345i sdfds".match(regex);
+
+  return result;
+});
 
 
